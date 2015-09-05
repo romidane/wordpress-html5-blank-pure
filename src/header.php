13c76c8181b5817baf04f6c5b13dd78647246ec9
Steven Bapaga <sbapaga@gmail.com>
2015-09-05 13:08:03 +0100

    Added the neccessary files for theme to work on sever

---
+++ src/header.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html <?php language_attributes(); ?> class="no-js">
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="description" content="<?php bloginfo('description'); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' : '; } ?><?php bloginfo('name'); ?></title>
+
+		<link href="//www.google-analytics.com" rel="dns-prefetch">
+    <link href="<?php echo get_template_directory_uri(); ?>/images/icons/favicon.ico" rel="shortcut icon">
+
+		<!-- Add to homescreen -->
+		<link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/manifest.json">
+
+		<!-- Fallback to homescreen for Chrome <39 on Android -->
+		<meta name="mobile-web-app-capable" content="yes">
+		<meta name="application-name" content="Web Starter Kit">
+		<link rel="icon" sizes="192x192" href="<?php echo get_template_directory_uri(); ?>/images/touch/chrome-touch-icon-192x192.png">
+
+		<!-- Add to homescreen for Safari on iOS -->
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+		<meta name="apple-mobile-web-app-title" content="Web Starter Kit">
+		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/images/touch/apple-touch-icon.png">
+
+		<!-- Tile icon for Win8 (144x144 + tile color) -->
+		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/images/touch/ms-touch-icon-144x144-precomposed.png">
+		<meta name="msapplication-TileColor" content="#3372DF">
+
+		<meta name="theme-color" content="#3372DF">
+
+		<?php wp_head(); ?>
+
+		<script>
+        // conditionizr.com
+        // configure environment tests
+        conditionizr.config({
+            assets: '<?php echo get_template_directory_uri(); ?>',
+            tests: {}
+        });
+    </script>
+
+	</head>
+	<body <?php body_class(); ?>>
+
+		<!-- wrapper -->
+		<div class="wrapper">
+
+			<!-- header -->
+			<header class="header clear" role="banner">
+
+					<!-- logo -->
+					<div class="logo">
+						<a href="<?php echo home_url(); ?>">
+							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
+							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
+						</a>
+					</div>
+					<!-- /logo -->
+
+					<!-- nav -->
+					<nav class="nav" role="navigation">
+						<?php html5blank_nav(); ?>
+					</nav>
+					<!-- /nav -->
+
+			</header>
+			<!-- /header -->
